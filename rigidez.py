ddd3f4db4d4eb25d902998aea3eef482ba662aa9
Mauro de Vasconcellos Real <mvrealgm@gmail.com>
2024-07-23 10:59:46 -0300

    wind transv

--- rigidez.py
+++ rigidez.py
@@ -3,39 +3,38 @@
 import numpy as np
 import pandas as pd
 
-k = np.zeros(4)
 kp = np.zeros(4)
 kn = np.zeros(4)
-deltap = np.zeros(4)
-deltan = np.zeros(4)
+k = np.zeros(4)
 
-Ec = 21.e6 # kN/m2
-Gn = 1000. # kN/m2
-d = 1.00 # m
-L = np.array([8.00, 10.00, 8.00, 5.00]) # m
-hn = 0.024 # m
-An = 0.21814 # m2
+# Constantes
+Ec = 21e6  # kN/m2
+Gn = 1000.0  # kN/m2
+d = 1.0  # m
+L = np.array([8.0, 10.0, 8.0, 5.0])  # m
+hn = 0.024  # m
+An = 0.21814  # m2
 
-Ip = np.pi*d**4/64.
-kp = 3. * Ec * Ip /L**3
-deltap = 1./kp
-kn[0] = Gn*An / hn
-kn[3] = kn[0]
-deltan[0] = 1./ kn[0]
-deltan[3] = 1./ kn[3]
-k = 1./(deltan+deltap)
+# Cálculos
+Ip = np.pi * d**4 / 64.0
+kp = 3.0 * Ec * Ip / L**3
+kn = [Gn * An / hn, 0.00, 0.00, Gn * An / hn] # Pilares P2 e P3 sem aparelho de neoprene
+deltap = 1.0 / kp
+deltan = [hn / (Gn * An), 0.00, 0.00, hn / (Gn * An)] # Pilares P2 e P3 sem aparelho de neoprene
+k = 1.0 / (deltan + deltap)
 
-print('Pilar    L(m)   kp(kN/m)   kn(kN/m)    k(kN/m) ')
-                      
-datadict = {'pilar': ['P1','P2','P3','P4'], 'altura': L, 'rig_pilar': kp, 'rig_neoprene': kn,'rigidez': k}
+# Criação do DataFrame
+datadict = {
+    'pilar': ['P1', 'P2', 'P3', 'P4'],
+    'altura': L,
+    'rigidez_pilar': kp,
+    'rigidez_neoprene': kn,
+    'rigidez_total': k
+}
 data = pd.DataFrame(datadict)
 
-# Imprimindo o DataFrame sem nomes de colunas e índices de linhas, com formatação
-formatted_data = data.copy()  # Criamos uma cópia para não modificar o DataFrame original
-formatted_data['altura'] = formatted_data['altura'].apply(lambda x: "{:10.2f}".format(x))
-formatted_data['rig_pilar'] = formatted_data['rig_pilar'].apply(lambda x: "{:10.2f}".format(x))
-formatted_data['rig_neoprene'] = formatted_data['rig_neoprene'].apply(lambda x: "{:10.2f}".format(x))
-formatted_data['rigidez'] = formatted_data['rigidez'].apply(lambda x: "{:10.2f}".format(x))
-
-print(formatted_data.to_string(index=False, header=False))
-
+# Imprimindo o DataFrame formatado
+print("Tabela 5.6.2 - Rigidezes dos pilares da ponte")
+print("Pilar    L(m)   kp(kN/m)     kn(kN/m)      k(kN/m)")
+print("-" * 55)  # Linha separadora
+print(data.to_string(index=False, header=False, float_format="{:10.2f}".format))
